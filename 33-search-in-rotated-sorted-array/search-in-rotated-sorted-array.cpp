38a1078e6b5a34a92f25834a3defcdad6b01c207
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-05-29 21:39:53 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (33.67%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int low=0;
-        int high=nums.size()-1;
+        int n=nums.size();
+        int high=n-1;
         int mid;
         while(low<=high){
             mid=(low+high)/2;
@@ -11,19 +12,15 @@ public:
                 if(target>=nums[low] && target<nums[mid]){
                     high=mid-1;
                 }
-                else{
-                    low=mid+1;
-                }
+                else low=mid+1;
             }
             else{
-                 if(target>nums[mid] && target<=nums[high]){
+                if(target>nums[mid] && target<=nums[high]){
                     low=mid+1;
                 }
-                else{
-                    high=mid-1;
-                }
+                else high=mid-1;
             }
         }
-        return -1;
+    return -1;
     }
 };
