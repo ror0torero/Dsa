36d3ec951f966302bdde5a9697d953f9a59a4a27
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 23:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (63.17%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int n=timeSeries.size();
+        int res=0;
+        for(int i=0;i<n-1;i++){
+            res+=min(duration,timeSeries[i+1]-timeSeries[i]);
+        }
+        return res+duration;
+    }
+};
