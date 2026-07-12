17bf957d8ae7842a7604bedbd80f16adf4317ae1
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 21:15:51 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.29%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,12 +4,9 @@ public:
         int n=nums.size();
         int i=0;
         for(int j=0;j<n;j++){
-            if(nums[j]!=0){
-                nums[i]=nums[j];
-                i++;
-            }
+            if(nums[j]!=0) {nums[i]=nums[j]; i++;}
         }
-        for(i;i<n;i++){
+        for(;i<n;i++){
             nums[i]=0;
         }
     }
