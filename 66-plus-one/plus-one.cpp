c3ebfa9bb80e4348e79f9de0dd9cb6d6e3e8494a
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 19:07:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (27.74%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -3,16 +3,12 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int n=digits.size();
         for(int i=n-1;i>=0;i--){
-            if(digits[i]!=9) {
-                digits[i]++;
-                break;
-            }
-            else{
+            if(digits[i]==9) {
                 digits[i]=0;
             }
-            if(i==0)
-            digits.insert(digits.begin(),1);
+            else {digits[i]++; break;}
         }
+        if(digits[0]==0) digits.insert(digits.begin(),1);
         return digits;
     }
 };
