c77f9f704b4709708c30c19c024fa96d335a6e75
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 22:44:22 +0530

    Time: 1 ms (77.84%) | Memory: 102.1 MB (44.52%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -3,23 +3,15 @@ public:
     int islandPerimeter(vector<vector<int>>& grid) {
         int r=grid.size();
         int c=grid[0].size();
-        vector<int> row(c,0);
-        grid.push_back(row);
-        grid.insert(grid.begin(),row);
-        for(auto &row:grid){
-            row.insert(row.begin(),0);
-            row.push_back(0);
-        }
-        r+=2;c+=2;
         int res=0;
-        for(int i=1;i<r-1;i++){
-            for(int j=1;j<c-1;j++){
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
                 if (grid[i][j] == 1) {
-                if (grid[i-1][j] == 0) res++;
-                if (grid[i+1][j] == 0) res++;
-                if (grid[i][j-1] == 0) res++;
-                if (grid[i][j+1] == 0) res++;
-}
+                if (i==0||grid[i-1][j] == 0) res++;
+                if (i==r-1||grid[i+1][j] == 0) res++;
+                if (j==0||grid[i][j-1] == 0) res++;
+                if (j==c-1||grid[i][j+1] == 0) res++;
+                }
             }
         }
         return res;
