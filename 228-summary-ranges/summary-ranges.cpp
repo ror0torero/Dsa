684515b8bae30d3317c043e1a8686a38c0e69513
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 21:01:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (41.90%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+
+        int i = 0;
+        while (i < nums.size()) {
+            int start = i;
+
+            while (i + 1 < nums.size() && nums[i + 1] == nums[i] + 1)
+                i++;
+
+            if (start == i)
+                res.push_back(to_string(nums[start]));
+            else
+                res.push_back(to_string(nums[start]) + "->" + to_string(nums[i]));
+
+            i++;
+        }
+
+        return res;
+    }
+};
