5c44b2bc2d02238202cf83e5e69d921876105112
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-12 20:23:09 +0530

    Time: 1988 ms (5.01%) | Memory: 73.9 MB (99.90%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=1;j<=k && i+j<n;j++)
+            if(nums[i]==nums[i+j]) return true;
+        }
+        return false;
+    }
+};
