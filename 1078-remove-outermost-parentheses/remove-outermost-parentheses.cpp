ccc2e5be6cb89140a163e2b58d66ff22a0bfe227
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-15 18:48:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (54.97%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -4,14 +4,14 @@ public:
         //using count?
         int n=s.size();
         string res="";
-        int count=0;
+        int count=0; //represent no. of open brackets ()
         for(int i=0;i<n;i++){
             if(s[i]=='(' && count==0) count++;
             else if(s[i]=='(' && count>0){
                 res+=s[i];
                 count++;
             }
-            else if(s[i]==')' && count>1){
+            else if(s[i]==')' && count>1){//only when no.of open brackets>1 we++
                 res+=s[i];
                 count--;
             }
