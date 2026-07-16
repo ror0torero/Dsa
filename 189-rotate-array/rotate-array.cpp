69ccbfa3efe333921f54378f897b8ef690bc3291
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-16 20:01:20 +0530

    Time: 0 ms (100.00%) | Memory: 29.9 MB (29.16%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,8 +3,16 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+        if(k==0) return;
+        int arr[k];
+        for(int i=n-1;i>=n-k;i--){
+            arr[n-1-i]=nums[i];
+        }
+        for(int i=n-1;i>=k;i--){
+            nums[i]=nums[i-k];
+        }
+        for(int i=k-1;i>=0;i--){
+            nums[k-1-i]=arr[i];
+        }
     }
 };
