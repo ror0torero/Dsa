bcca05fd4c04e11bc61554c657e589c0b3f33d23
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-26 21:09:35 +0530

    Time: 3 ms (23.81%) | Memory: 13.5 MB (67.72%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool checkIfExist(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for(auto it:nums) cout<<it<<" ";
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
