4276c63c7cb60dfce3b300acd26735fe279f4a9a
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-07-26 21:28:09 +0530

    Time: 27 ms (8.41%) | Memory: 17.6 MB (99.83%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
         sort(nums.begin(),nums.end());
-        vector<int> res;
-        for(int i=0;i<queries.size();i++){
-            if(nums[0]>queries[i]) res.push_back(0);
-            else{
-                int sum=0;
-                int j=0;
-                while(sum<queries[i] && j<nums.size()){
-                    sum+=nums[j];
-                    if(sum>queries[i]) break;
-                    j++;
-                }
-                res.push_back(j);
-                }
+        for(auto &it:queries){
+            int sum=0;
+            int c=0;
+            for(auto &i:nums){
+                if(sum+i<=it) {sum+=i; c++;}
             }
-        return res;
+            it=c;
+        }
+        return queries;
     }
 };
