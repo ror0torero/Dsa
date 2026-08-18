bf4f79d2006e902428c9cdf30a9ee5d0594c5433
Rohan Agarwal <rohan.agarwal16@outlook.com>
2026-08-18 16:47:05 +0530

    Time: 93 ms (5.01%) | Memory: 23.8 MB (82.63%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        //overlap occurs when interval 2nd ele is smaller than another intervals first ele.
+        sort(intervals.begin(),intervals.end(),[](const vector<int>&a,const vector<int> &b){
+            if(a[0]!=b[0])
+            return(a[0]<b[0]);
+            else{
+                return (a[1]<b[1]);
+            }
+        });
+        vector<vector<int>> res;
+        int n=intervals.size();
+        int i=0,j=1,m;
+        bool merge=false;
+        while(i<n){
+            m=intervals[i][1];
+            if(j<n && intervals[i][1]>=intervals[j][0]){
+                merge=true;
+                m=max(m,intervals[j][1]);
+                j++;
+                while(j<n && m>=intervals[j][0]){
+                m=max(m,intervals[j][1]);
+                j++;
+                cout<<i<<" "<<j<<endl;
+                }
+            }
+            if(merge){
+                vector<int> a;
+                a.push_back(intervals[i][0]);
+                a.push_back(m);
+                res.push_back(a);
+                merge=false;
+                i=j;
+                j=i+1;
+                cout<<i<<" "<<j<<endl;
+            }
+            else{
+                cout<<i<<" "<<j<<endl;
+                res.push_back(intervals[i]);
+                i++;
+                j=i+1;
+            }
+            
+        }
+        return res;
+    }
+};
